ed793935c5b3bb2f28818bd548d15dc3b0e29fb9
MainMemory <mainmemory@yahoo.com>
2019-03-22 12:23:38 -0500

    Updating to use new SMPSPlay-DLL API.

--- Mania-SMPS-Mod/ManiaSMPSMod.cpp
+++ Mania-SMPS-Mod/ManiaSMPSMod.cpp
@@ -9,11 +9,12 @@
 using std::string;
 using std::unordered_map;
 
-BOOL(*PlaySong)(short song);
+BOOL(*LoadSong)(short song);
+BOOL(*PlaySong)();
 BOOL(*StopSong)();
 BOOL(*PauseSong)();
 BOOL(*ResumeSong)();
-BOOL(*SetSongTempo)(unsigned int pct);
+BOOL(*SetSongTempo)(double multi);
 void(*SetVolume)(double vol);
 
 struct struct_0
@@ -76,7 +77,8 @@ int PlayMusicFile_r(char *name, unsigned int slot, int a3, unsigned int loopstar
 		stru_26B818[slot].anonymous_1 = 0;
 		stru_26B818[slot].anonymous_3 = 0x10000;
 
-		PlaySong(choice->second);
+		LoadSong(choice->second);
+		PlaySong();
 		stru_26B818[slot].playStatus = 2;
 		one_up = !_stricmp(name, "1up.ogg");
 		if (!_stricmp(name, "bluespheresspd.ogg"))
@@ -91,12 +93,12 @@ int PlayMusicFile_r(char *name, unsigned int slot, int a3, unsigned int loopstar
 
 void SpeedUpMusic()
 {
-	SetSongTempo(66);
+	SetSongTempo(1.5);
 }
 
 void SlowDownMusic()
 {
-	SetSongTempo(100);
+	SetSongTempo(1);
 }
 
 VoidFunc(ResumeSound, 0x001BC9C0);
@@ -113,170 +115,6 @@ void PauseSound_SMPS()
 	PauseSound();
 }
 
-const string SMPSMusicList[] = {
-	"S3Title",
-	"AngelIsland1",
-	"AngelIsland2",
-	"Hydrocity1",
-	"Hydrocity2",
-	"MarbleGarden1",
-	"MarbleGarden2",
-	"CarnivalNight1",
-	"CarnivalNight2",
-	"FlyingBattery1",
-	"FlyingBattery2",
-	"IceCap1",
-	"IceCap2",
-	"LaunchBase1",
-	"LaunchBase2",
-	"MushroomHill1",
-	"MushroomHill2",
-	"Sandopolis1",
-	"Sandopolis2",
-	"LavaReef1",
-	"LavaReef2",
-	"SkySanctuary",
-	"DeathEgg1",
-	"DeathEgg2",
-	"SKMidboss",
-	"Boss",
-	"Doomsday",
-	"MagneticOrbs",
-	"SpecialStage",
-	"SlotMachine",
-	"GumballMachine",
-	"S3Knuckles",
-	"AzureLake",
-	"BalloonPark",
-	"DesertPalace",
-	"ChromeGadget",
-	"EndlessMine",
-	"GameOver",
-	"Continue",
-	"ActClear",
-	"S31Up",
-	"ChaosEmerald",
-	"S3Invincibility",
-	"CompetitionMenu",
-	"S3Midboss",
-	"LevelSelect",
-	"FinalBoss",
-	"Drowning",
-	"S3AllClear",
-	"S3Credits",
-	"SKKnuckles",
-	"SKTitle",
-	"SK1Up",
-	"SKInvincibility",
-	"SKAllClear",
-	"SKCredits",
-	"S3CCredits",
-	"S3Continue",
-	"SKTitle0525",
-	"SKAllClear0525",
-	"SKCredits0525",
-	"GreenGrove1",
-	"GreenGrove2",
-	"RustyRuin1",
-	"RustyRuin2",
-	"VolcanoValley2",
-	"VolcanoValley1",
-	"SpringStadium1",
-	"SpringStadium2",
-	"DiamondDust1",
-	"DiamondDust2",
-	"GeneGadget1",
-	"GeneGadget2",
-	"PanicPuppet2",
-	"FinalFight",
-	"S3DEnding",
-	"S3DSpecialStage",
-	"PanicPuppet1",
-	"S3DBoss2",
-	"S3DBoss1",
-	"S3DIntro",
-	"S3DCredits",
-	"S3DInvincibility",
-	"S3DMenu",
-	"S4E1Boss",
-	"GreenHill",
-	"Labyrinth",
-	"Marble",
-	"StarLight",
-	"SpringYard",
-	"ScrapBrain",
-	"S1Invincibility",
-	"S11Up",
-	"S1SpecialStage",
-	"S1Title",
-	"S1Ending",
-	"S1Boss",
-	"FinalZone",
-	"S1ActClear",
-	"S1GameOver",
-	"S1Continue",
-	"S1Credits",
-	"S1Drowning",
-	"S1ChaosEmerald",
-	"CasinoNight2P",
-	"EmeraldHill",
-	"Metropolis",
-	"CasinoNight",
-	"MysticCave",
-	"MysticCave2P",
-	"AquaticRuin",
-	"S2DeathEgg",
-	"S2SpecialStage",
-	"S2Options",
-	"S2Ending",
-	"S2FinalBoss",
-	"ChemicalPlant",
-	"S2Boss",
-	"SkyChase",
-	"OilOcean",
-	"WingFortress",
-	"EmeraldHill2P",
-	"S22PResults",
-	"S2SuperSonic",
-	"HillTop",
-	"S2Title",
-	"S2Invincibility",
-	"S2HiddenPalace",
-	"S2Credits",
-	"CasinoNight2PBeta",
-	"EmeraldHillBeta",
-	"MetropolisBeta",
-	"CasinoNightBeta",
-	"MysticCaveBeta",
-	"MysticCave2PBeta",
-	"AquaticRuinBeta",
-	"S2DeathEggBeta",
-	"S2SpecialStageBeta",
-	"S2OptionsBeta",
-	"S2FinalBossBeta",
-	"ChemicalPlantBeta",
-	"S2BossBeta",
-	"SkyChaseBeta",
-	"OilOceanBeta",
-	"WingFortressBeta",
-	"EmeraldHill2PBeta",
-	"S22PResultsBeta",
-	"S2SuperSonicBeta",
-	"HillTopBeta",
-	"S3DSpecialStageBeta",
-	"CarnivalNight1PC",
-	"CarnivalNight2PC",
-	"IceCap1PC",
-	"IceCap2PC",
-	"LaunchBase1PC",
-	"LaunchBase2PC",
-	"KnucklesPC",
-	"CompetitionMenuPC",
-	"UnusedPC",
-	"CreditsPC",
-	"S3InvincibilityPC"
-};
-
 unordered_map<string, short> songmap;
 
 DataPointer(MusicInfo *, MusicSlots, 0xAC6E08);
@@ -288,6 +126,8 @@ void SongStoppedCallback()
 		oldstatus = stru_26B818[oldsong].playStatus = 0;
 	one_up = false;
 }
+
+double bluespheretempos[]{ 1, 1.086956521739130434782608, 1.19047619047619047619, 1.31578947368421052631, 1.470588235294117647 };
 
 extern "C"
 {
@@ -319,29 +159,33 @@ extern "C"
 			oldstatus = status;
 			SetVolume(stru_26B818[song].volume * 0.5 * MusicVolume);
 			if (bluespheretime != -1 && bluespheretime < 7200 && status == 2 && ++bluespheretime % 1800 == 0)
-				SetSongTempo(100 - (8 * bluespheretime / 1800));
+				SetSongTempo(bluespheretempos[bluespheretime / 1800]);
 		}
 		else
 			oldstatus = 0;
 		oldsong = song;
 	}
 
-#define GetFunc(name) name = (decltype(name))GetProcAddress(handle, #name)
+#define GetFunc(name) name = (decltype(name))GetProcAddress(handle, "SMPS_"#name)
 	__declspec(dllexport) void Init(const char *path)
 	{
 		char pathbuf[MAX_PATH];
 		strcpy_s(pathbuf, path);
-		strcat_s(pathbuf, "\\SMPSOUT.dll");
+		strcat_s(pathbuf, "\\SMPSPlay.dll");
 		HMODULE handle = LoadLibraryA(pathbuf);
 		if (!handle)
 		{
-			PrintDebug("Mania-SMPS-Mod: Could not load SMPSOUT.dll!\n");
+			PrintDebug("Mania-SMPS-Mod: Could not load SMPSPlay.dll!\n");
 			return;
 		}
-		BOOL(*InitializeDriver)() = GetFunc(InitializeDriver);
-		void(*RegisterSongStoppedCallback)(void(*callback)) = GetFunc(RegisterSongStoppedCallback);
-		const char **(*GetCustomSongs)(unsigned int &count) = GetFunc(GetCustomSongs);
+		BOOL(*InitializeDriver)();
+		GetFunc(InitializeDriver);
+		void(*RegisterSongStoppedCallback)(void(*callback));
+		GetFunc(RegisterSongStoppedCallback);
+		const char **(*GetSongNames)(unsigned int &count);
+		GetFunc(GetSongNames);
 		GetFunc(SetVolume);
+		GetFunc(LoadSong);
 		GetFunc(PlaySong);
 		GetFunc(StopSong);
 		GetFunc(PauseSong);
@@ -354,13 +198,10 @@ extern "C"
 		SetCurrentDirectoryA(olddir);
 		RegisterSongStoppedCallback(SongStoppedCallback);
 		SetVolume(0.5 * MusicVolume);
-		size_t sng;
-		for (sng = 0; sng < LengthOfArray(SMPSMusicList); sng++)
-			songmap[SMPSMusicList[sng]] = (short)sng;
 		unsigned int custcnt;
-		const char **cust = GetCustomSongs(custcnt);
+		const char **cust = GetSongNames(custcnt);
 		for (unsigned int i = 0; i < custcnt; i++)
-			songmap[cust[i]] = (short)(i + sng);
+			songmap[cust[i]] = (short)i;
 		strcpy_s(pathbuf, path);
 		strcat_s(pathbuf, "\\config.ini");
 		const IniFile *cfg = new IniFile(pathbuf);
